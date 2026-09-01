e6e4239f64fb6bfa4c29bbb9241fb68821ecc970
Rocky Kumar Ram <rockykumarramofficial@gmail.com>
2026-09-02 01:38:17 +0530

    Add problem link to combine two tables SQL file
    Added problem link for reference.

--- Easy/175_combine_two_tables.sql
+++ Easy/175_combine_two_tables.sql
@@ -1,5 +1,6 @@
 -- Problem: 175. Combine Two Tables
 -- Platform: LeetCode
+-- Link: https://leetcode.com/problems/combine-two-tables/
 -- Difficulty: Easy
 
 SELECT Person.firstName, Person.lastName, Address.city, Address.state
